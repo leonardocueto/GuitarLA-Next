50e63b43981106b31924b5fee9aa670e59ab6ed9
Leonardo Cueto <leocueto1999@gmail.com>
2023-08-12 11:16:16 -0300

    Se agrego componente Layout.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-
+import Link from "next/link"
+import Layout from "../components/layout"
 export default function Home() {
   return (
     <>
-
-    <h1>Hola Mundo</h1>
-    
+    <Layout>
+      <h1>Hola Mundo</h1>
+      <Link href='/nosotros'>Nosotros</Link>
+    </Layout>
     </>
   )
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import Layout from "../components/layout"
+
+export default function Nosotros() {
+  return (
+    <Layout>
+      
+      <Link href='/'>Ir a Inicio</Link>
+    </Layout>
+  )
+}
