a8b315ce4bb6222d802cbff4dd101f19c05967e0
Leonardo Cueto <leocueto1999@gmail.com>
2023-08-14 13:20:50 -0300

    se agregaron estilos nosotros y modificaron header y tooter

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,28 @@
+import Link from "next/link";
+import styles from '../styles/footer.module.css'
+
 
 export default function Footer() {
   return (
-    <div>
-      footer
-    </div>
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+      <nav className={styles.navegacion}>
+          <Link href='/' >
+            Inicio
+          </Link>
+          <Link href='/nosotros' >
+            Nosotros
+          </Link>
+          <Link href='/tienda' >
+            Tienda
+          </Link>
+          <Link href='/blog' >
+            Blog
+          </Link>
+
+        </nav>
+        <p className={styles.copyright}>todos los derechos reservados {new Date().getFullYear()}</p>
+      </div>
+    </footer>
   )
 }
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,14 +1,43 @@
-
-import Layout from "../components/layout"
+import Image from 'next/image'
+import Layout from '../components/layout'
+import styles from '../styles/nosotros.module.css'
 
 export default function Nosotros() {
   return (
     <Layout
       title={'Nosotros'}
-      description="Sobre nosotros, guitarLa, tienda de música"
+      description='Sobre nosotros, guitarLa, tienda de música'
     >
-      
-     <h1>Nosotros</h1>
+      <main className='contenedor'>
+        <h2 className='heading'>Nosotros</h2>
+        <div className={styles.contenido}>
+          <Image
+            src='/img/nosotros.jpg'
+            alt='Imagen sobre nosotros'
+            width={1000}
+            height={800}
+          />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              dictum eu mauris vel venenatis. Mauris nec purus sed purus mollis
+              luctus. Sed vitae lacus accumsan, molestie elit eu, posuere urna.
+              Mauris pulvinar orci accumsan tortor consectetur, id pellentesque
+              lacus dapibus. Interdum et malesuada fames ac ante ipsum primis in
+              faucibus.
+            </p>
+            <p>
+              Quisque lacus enim, vehicula sit amet arcu ac, accumsan commodo
+              elit. Vestibulum ante ipsum primis in faucibus orci luctus et
+              ultrices posuere cubilia curae; Duis efficitur lacus eget feugiat
+              rutrum. Quisque vel nisl rhoncus, molestie enim a, cursus justo.
+              In id cursus risus. Proin consectetur est porta lacinia egestas.
+              Nullam consectetur felis at sem vulputate, sit amet rutrum nibh
+              porttitor.
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
