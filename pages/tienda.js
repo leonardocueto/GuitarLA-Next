2069aaa5adcd263f20aea2891e2535d2a889675a
Leonardo Cueto <leocueto1999@gmail.com>
2023-08-14 21:18:14 -0300

    fetch api + getserversideprops

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout'
 import ListadoGuitarras from '../components/listado-guitarras'
-export default function Tienda() {
+export default function Tienda({ guitarras }) {
+  console.log(guitarras)
   return (
     <Layout
       title={'Tienda Virtual'}
@@ -12,4 +13,32 @@ export default function Tienda() {
       </main>
     </Layout>
   )
+}
+
+/* export async function getStaticProps() {
+  //se ejecuta primero "servidor" > es necesario un build por cambio
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  )
+  const { data: guitarras } = await respuesta.json()
+
+  return {
+    props: {
+      guitarras
+    }
+  }
+} */
+
+export async function getServerSideProps() {
+  // forma dinamica
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  )
+  const { data: guitarras } = await respuesta.json()
+
+  return {
+    props: {
+      guitarras
+    }
+  }
 }
