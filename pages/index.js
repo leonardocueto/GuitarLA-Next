195e5c0527e8fedc05f318927ccfc70fb0892818
Leonardo Cueto <leocueto1999@gmail.com>
2023-08-17 11:30:12 -0300

    agregando routing dinamico blog y modificando index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,57 @@
-
-import Layout from "../components/layout"
-export default function Home() {
+import Guitarra from '../components/guitarra'
+import Layout from '../components/layout'
+import Post from '../components/post'
+import styles from '../styles/grid.module.css'
+export default function Home({ guitarras, posts }) {
   return (
     <>
-    <Layout 
-      title={'Inicio'}
-      description="Blog de música, ventas de guitarras y mas"
-    >
-      <h1>Hola Mundo</h1>
-
-    </Layout>
+      <Layout
+        title={'Inicio'}
+        description='Blog de música, ventas de guitarras y mas'
+      >
+        <main className='contenedor'>
+          <h1 className='heading'>Nuestra Coleccion</h1>
+          <div className={styles.grid}>
+            {guitarras?.map((guitarra) => (
+              <Guitarra
+                key={guitarra.id}
+                guitarra={guitarra.attributes}
+              />
+            ))}
+          </div>
+        </main>
+        <section className='contenedor'>
+          <h2 className='heading'>Blog</h2>
+          <div className={styles.grid}>
+            {posts?.map((post) => (
+              <Post
+                key={post.id}
+                post={post.attributes}
+              />
+            ))}
+          </div>
+        </section>
+      </Layout>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const urlGuitarras = `${process.env.API_URL}/guitarras?populate=imagen`
+  const urlPosts = `${process.env.API_URL}/posts?populate=imagen`
+
+  const [resGuitarras, resPosts] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlPosts)
+  ])
+  const [{ data: guitarras }, { data: posts }] = await Promise.all([
+    resGuitarras.json(),
+    resPosts.json()
+  ])
+  return {
+    props: {
+      guitarras,
+      posts
+    }
+  }
 }
