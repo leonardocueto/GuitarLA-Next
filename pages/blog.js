ca52dbfa90aa332d62b4e85c1ca049f739d56db3
Leonardo Cueto <leocueto1999@gmail.com>
2023-08-17 11:04:42 -0300

    css en blog + formateo de fecha en util/helper

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,37 @@
+import Layout from '../components/layout'
+import Post from '../components/post'
+import styles from '../styles/grid.module.css'
 
-import Layout from "../components/layout"
-
-export default function Blog() {
+export default function Blog({ posts }) {
+  console.log(posts)
   return (
     <Layout
       title={'Blog'}
-      description="Blog de música, venta de guitarras, consejo, GuitarLA"
+      description='Blog de música, venta de guitarras, consejo, GuitarLA'
     >
-      
-     <h1>Blog</h1>
+      <main className='contenedor'>
+        <h1 className='heading'>Blog</h1>
+        <div className={styles.grid}>
+          {posts?.map((post) => (
+            <Post
+              key={post.id}
+              post={post.attributes}
+            />
+          ))}
+        </div>
+      </main>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  //se ejecuta primero "servidor" > es necesario un build por cambio
+  const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`)
+  const { data: posts } = await respuesta.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
